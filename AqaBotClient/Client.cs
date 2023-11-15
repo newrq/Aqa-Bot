7f63641ae2c628a992bdd8c231fbc0b4012f7faf
Annk <smile.sir666@gmail.com>
2023-11-15 16:51:08 +0200

    Player cant play bug... fix later

--- AqaBotClient/Client.cs
+++ AqaBotClient/Client.cs
@@ -3,73 +3,78 @@
 
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Unicode;
+using System.Threading.Channels;
 
 namespace AqaBotClient;
 
 public class Client : IDisposable
 {
     //TODO: make events 
+
     #region EventsAndEtc
 
-    public delegate  void MessageEventHandler(object sender, Message message); // Message event handler
-        
-    public event MessageEventHandler OnMessageRecived = (sender,msg) =>
+    public delegate void MessageEventHandler(object sender, Message message); // Message event handler
+
+    public event MessageEventHandler OnMessageRecived = (sender, msg) =>
     {
         Console.WriteLine("Message received by " + msg.GetAuthor() + " ---> " + msg.GetContent());
     }; // When message recived
-    
-    
+
+
     public event MessageEventHandler OnMessageError; // When message send error
 
 
     public delegate void ConnectionEventHandler(object sender);
-    public event ConnectionEventHandler OnConnectionSucces = (sender) => {
-        Console.WriteLine("Connection success!");
-    }; // When connection success
-    public event ConnectionEventHandler OnConnectionError = (sender) => throw new Exception("Connection error..."); // throw when connection error
+
+    public event ConnectionEventHandler
+        OnConnectionSucces = (sender) => { Console.WriteLine("Connection success!"); }; // When connection success
+
+    public event ConnectionEventHandler
+        OnConnectionError = (sender) => throw new Exception("Connection error..."); // throw when connection error
 
     #endregion
 
     #region server
 
-    
 
-    
-    public string Name = "Aqa Bot";
+
+
+    public string Name = "AqaBot";
     public string Ip;
     public int Port;
 
     #endregion
-    
-    private TcpClient _client = new TcpClient();
+
+    public TcpClient _client = new TcpClient();
     private NetworkStream _networkStream;
-    private bool _isDisposed=false;
-    
+    private bool _isDisposed = false;
+
     // TODO: make player Entity creating 
     public SelfPlayer PlayerEntity;
     public WorldInfo WorldInfo;
-    
-    
+
+
     // Initializing the Client class
-    public Client(string ip,int port)
+    public Client(string ip, int port)
     {
         this.Ip = ip;
         this.Port = port;
     }
-    
+
     // Initializing the Client class
     public Client(string ip, string name)
     {
         this.Ip = ip;
         this.Name = name;
     }
-    
+
     //TODO:Make Connecting to server
-    public async Task AsyncConnect() 
+    public async Task AsyncConnect()
     {
         try
         {
-            await _client.ConnectAsync(Ip,Port); // Connecting to server 1 attempt
+            await _client.ConnectAsync(Ip, Port); // Connecting to server 1 attempt
             _networkStream = _client.GetStream();
 
         }
@@ -88,24 +93,32 @@ public class Client : IDisposable
                 await AsyncHandShakeSend();
                 Console.WriteLine("Starting login");
                 await AsyncLoginStart();
-                Console.WriteLine("Play package send");
-                await PlayPackageSend();
+                await Task.Delay(5000);
+                Console.WriteLine("Switch package send");   
+                await LoginAcknowledgedPackage();
+                Console.WriteLine("Sending config package");
+                await ConfigurationPackageSend();
+                await Task.Delay(10000);
+                Console.WriteLine("Sending configurationConfirm package");
+                await FinishConfigurationPackageSend();
+                Console.WriteLine("Spawning player");
+                await SpawnPlayer();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                throw;
+                Environment.Exit(1);
             }
         }
     }
-    
-    private void Disposing()    
+
+    private void Disposing()
     {
         if (_isDisposed)
         {
             Console.WriteLine("Client disposing...");
             return;
-            
+
         }
     }
 
@@ -115,101 +128,183 @@ public class Client : IDisposable
         _isDisposed = true;
         Disposing();
     }
-    
-    
+
+
     //TODO:Make first package send
     private async Task AsyncHandShakeSend()
     {
         using (MemoryStream memoryStream = new MemoryStream())
         {
+            await Task.Delay(1000);
             //Package id
-            WriteVarInt(memoryStream,0x00);
-            
-            
+            WriteVarInt(memoryStream, 0x00);
+
+            await Task.Delay(1000);
             //Protocol 
-            WriteVarInt(memoryStream,764);
-            
+            WriteVarInt(memoryStream, 764);
+
+            await Task.Delay(1000);
             // Server ip
             byte[] serverAddressBytes = Encoding.UTF8.GetBytes(Ip);
             WriteVarInt(memoryStream, serverAddressBytes.Length);
+            await Task.Delay(1000);
             memoryStream.Write(serverAddressBytes, 0, serverAddressBytes.Length);
-
-            
+            await Task.Delay(1000);
             //Server Port
             byte[] portBytes = BitConverter.GetBytes((ushort)Port);
             Array.Reverse(portBytes);
-            memoryStream.Write(portBytes, 0, portBytes.Length);
-            
-            // Status (1 - login)
+            await memoryStream.WriteAsync(portBytes, 0, portBytes.Length);
+
+            // Status (2 - login)
             WriteVarInt(memoryStream, 2);
+            await Task.Delay(1000);
             
             // Sending data on server
             byte[] packetData = memoryStream.ToArray();
+            await Task.Delay(1000);
             await _networkStream.WriteAsync(packetData, 0, packetData.Length);
         }
+        await ReadRecivedInfo(_networkStream,_client);
 
-        try
-        {
-            byte[] buffer = new byte[9000];
-            int bytesRead = await _networkStream.ReadAsync(buffer, 0, buffer.Length);
-            Console.WriteLine(Encoding.UTF8.GetString(buffer,0,bytesRead));
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message + "\n\n CANT READ");
-            throw;
-        }
-        
     }
-    
+
     private async Task AsyncLoginStart()
     {
         using (MemoryStream memoryStream = new MemoryStream())
         {
-           WriteVarInt(memoryStream,0x00); // Login package ID
-
-           byte[] nicknameBytes = Encoding.UTF8.GetBytes(Name); // nickname bytes
-           WriteVarInt(memoryStream,nicknameBytes.Length);
-
-           byte[] playerUIID = Encoding.UTF8.GetBytes(Name); // uiid bytes
-           WriteVarInt(memoryStream,playerUIID.Length);
-
-           byte[] packetData = memoryStream.ToArray();
-           await _networkStream.WriteAsync(packetData, 0, packetData.Length);
+            WriteVarInt(memoryStream, 0x00); // Login package ID
+            await Task.Delay(1000);
+            byte[] nicknameBytes = Encoding.UTF8.GetBytes(Name); // nickname bytes
+            WriteVarInt(memoryStream, nicknameBytes.Length);
+            await Task.Delay(1000);
+            Guid playerUIID = Guid.NewGuid();
+            byte[] playerUIIDbytes = playerUIID.ToByteArray(); // uiid bytes
+            WriteVarInt(memoryStream, playerUIIDbytes.Length);
+            await Task.Delay(1000);
+            byte[] packetData = memoryStream.ToArray();
+            await _networkStream.WriteAsync(packetData, 0, packetData.Length);
+            
         }
-        //
-        // byte[] buffer = new byte[3000];
-        // int bytesRead = await _networkStream.ReadAsync(buffer, 0, buffer.Length);
-        // Console.WriteLine(Encoding.UTF8.GetString(buffer,0,bytesRead));
+        await ReadRecivedInfo(_networkStream,_client);
     }
 
-    
-    //TODO: PLAYER PACKAGE SEND ERROR
-    private async Task PlayPackageSend()
+
+    //TODO: switch PACKAGE SEND ERROR
+    private async Task LoginAcknowledgedPackage() // This packet switches the connection state to configuration.
     {
         using (MemoryStream memoryStream = new MemoryStream())
         {
-           WriteVarInt(memoryStream,0x00); // Play package ID
-           byte[] playPackage = memoryStream.ToArray();
-           await _networkStream.WriteAsync(playPackage, 0, playPackage.Length);
+            WriteVarInt(memoryStream, 0x03); // Switch package ID
+            await Task.Delay(1000);
+
+            byte[] playPackage = memoryStream.ToArray();
+            await _networkStream.WriteAsync(playPackage, 0, playPackage.Length);
         }
-        byte[] buffer = new byte[3000];
-        int bytesRead = await _networkStream.ReadAsync(buffer, 0, buffer.Length);
-        Console.WriteLine(Encoding.UTF8.GetString(buffer,0,bytesRead));
+        await ReadRecivedInfo(_networkStream,_client);
     }
     
-    
+    private async Task ConfigurationPackageSend()
+    {
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            WriteVarInt(memoryStream, 0x00); // Switch package ID
+            await Task.Delay(1000);
+
+            WriteVarInt(memoryStream, Encoding.UTF8.GetBytes("en_GB").Length); // Locale
+            await Task.Delay(500);
+            WriteVarInt(memoryStream,16); // View distance
+            await Task.Delay(500);
+            WriteVarInt(memoryStream,0); // 	0: enabled, 1: commands only, 2: hidden
+            await Task.Delay(500);
+            WriteVarInt(memoryStream,0x01); // “Colors” multiplayer setting. Can the chat be colored? 0x01 - true 0x00 - false
+            await Task.Delay(500);
+            WriteVarInt(memoryStream,0x40); // Displayed Skin Parts		Bit mask, see below.
+            
+            /*Bit 0 (0x01): Cape enabled
+             *Bit 1 (0x02): Jacket enabled
+             *Bit 2 (0x04): Left Sleeve enabled
+             *Bit 3 (0x08): Right Sleeve enabled
+             *Bit 4 (0x10): Left Pants Leg enabled
+             *Bit 5 (0x20): Right Pants Leg enabled
+             *Bit 6 (0x40): Hat enabled   
+             */
+            
+            await Task.Delay(500);
+            WriteVarInt(memoryStream,1); // Main Hand .... 0: Left, 1: Right.
+            await Task.Delay(500);
+            WriteVarInt(memoryStream,0x00); // Enable text filtering (BOOL)
+            await Task.Delay(500);
+            WriteVarInt(memoryStream,0x01); // Allow server listings... Servers usually list online players, this option should let you not show up in that list.
+            await Task.Delay(500);
+            byte[] playPackage = memoryStream.ToArray();
+            await _networkStream.WriteAsync(playPackage, 0, playPackage.Length);
+        }
+        await ReadRecivedInfo(_networkStream,_client);
+    }
+
+    private async Task FinishConfigurationPackageSend()
+    {
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            WriteVarInt(memoryStream, 0x02); // Switch package ID
+            await Task.Delay(1000);
+            byte[] playPackage = memoryStream.ToArray();
+            await _networkStream.WriteAsync(playPackage, 0, playPackage.Length);
+        }
+        await ReadRecivedInfo(_networkStream,_client);
+    }
+
+
+    private async Task SpawnPlayer()
+    {
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            WriteVarInt(memoryStream,0x00);
+            
+            byte[] packetData = memoryStream.ToArray();
+            await _networkStream.WriteAsync(packetData, 0, packetData.Length);
+        }
+
+        ReadRecivedInfo(_networkStream, _client);
+    }
+
     static void WriteVarInt(Stream stream, int value)
     {
         do
         {
             byte temp = (byte)(value & 0b01111111);
-            value >>= 7; 
+            value >>= 7;
             if (value != 0)
             {
                 temp |= 0b10000000;
             }
+
             stream.WriteByte(temp);
-        } while (value != 0); 
+        } while (value != 0);
     }
+
+    private static async Task ReadRecivedInfo(NetworkStream _networkStream,TcpClient _client)
+    {
+        if (_client.Connected)
+        {
+            await Task.Delay(3000);
+            if (_networkStream.DataAvailable)
+            {
+                byte[] buffer = new byte[10000];
+                int bytesRead = await _networkStream.ReadAsync(buffer, 0, buffer.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+            else
+            {
+                Console.WriteLine("No data");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not connected");
+        }
+        
+    }
+
+
 }
--- AqaBotClient/Program.cs
+++ AqaBotClient/Program.cs
@@ -3,11 +3,16 @@
 
 abstract class Program
 {
-    private static  Client AqaBot = new Client("localhost",1111); // Testing IP
+    private static  Client AqaBot = new Client("26.17.199.154",1111); // Testing IP
     public static async Task Main(string[] args)
     {
         await AqaBot.AsyncConnect();
-        await Task.Delay(-1);
+        while(AqaBot._client.Connected)
+        {
+            await Task.Delay(5000);
+        }
+
+        Console.WriteLine("AQA BOT DISCONNECTED");
         
     }
     
